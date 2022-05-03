757908006cbdc5d91a03ea55d6c48d414d454d22
Berkay ÇIRAK <81809211+berkaycirak@users.noreply.github.com>
2022-05-03 09:05:30 +0300

    Add files via upload
    New pages

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,24 @@
 const heartIcon = document.querySelectorAll(".icon-right");
+const hamburgerMenu = document.querySelector(".toggle");
+const headerMenu = document.querySelector(".header__menu");
+const hamburger = document.querySelector(".hamburger");
+const topButton = document.querySelector(".top-button");
 
 heartIcon.forEach((icon) => {
   icon.addEventListener("click", () => {
     icon.classList.toggle("active");
   });
+});
+
+hamburgerMenu.addEventListener("click", () => {
+  headerMenu.classList.toggle("active");
+  hamburger.classList.toggle("opened");
+});
+
+window.addEventListener("scroll", () => {
+  if (scrollY > 600) {
+    topButton.classList.add("active");
+  } else {
+    topButton.classList.remove("active");
+  }
 });
